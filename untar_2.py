078712dcd30fc55db59ef5c7fef0caad61350fdf
zapcom technologies <lincytv@gmail.com>
2018-10-11 02:41:53 +0530

    Add files via upload

---
+++ untar_2.py
@@ -0,0 +1,80 @@
+import boto3
+import tarfile
+import os
+
+# variable 
+s3bucket = "lincytest"
+dirname = "s3upload"
+
+apath = "/home/lincytv/scripts/s3upload/*"
+
+tarfilename = "sample.tar.gz"
+filename = []
+master_bucket = "lincyfinal"
+
+f = open('s3uploadedlist.log', 'a+')
+
+# functions 
+
+def extract(dirname, tarfilename):
+    # dirname = dirname
+    # tarfilename = tarfilename
+    tf = tarfile.open(tarfilename, "r")
+    tf.extractall(path=dirname)
+
+
+# def upload_file(master_bucket,filename):
+#     print filename
+#     try:
+#         conn.put_object(master_bucket, filename)
+#         f.write(filename)
+#     except Exception as e:
+#         print e
+#         pass
+
+def upload_file(master_bucket,filename):
+    print filename
+    try:
+        conn.put_object(master_bucket, filename)
+        f.write(filename)
+    except Exception as e:
+        print e
+        pass
+
+conn = boto3.client('s3')
+
+conn2 = boto3.resource('s3')
+
+oblist = conn.list_objects_v2(Bucket= s3bucket)
+for i in oblist["Contents"]:
+    m = i["Key"]
+    if m.endswith('.gz'):
+        conn.download_file(s3bucket,i["Key"],i["Key"])
+        try:
+            extract(dirname,i["Key"])
+        except Exception as e:
+            print e
+            pass
+
+fileextrace = os.listdir(dirname)
+
+for i in fileextrace:
+    try:
+        conn.upload_file(apath, master_bucket, i)
+        #conn2.Bucket(master_bucket).put_object(Key = i)
+fileextrace = os.listdir("s3upload")
+os.chdir("s3upload")
+for i in fileextrace:
+    try:
+        conn.put_object(master_bucket, i)
+
+        f.write(i)
+    except Exception as e:
+        print e
+        pass
+    #print i
+    #upload_file(master_bucket, i )
+    #print fileextrace
+
+
+f.close()
